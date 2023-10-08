29eb4538ff03c913108797b2274b9f4043d7505d
Jerome Dubus <dubus.jerome@gmail.com>
2023-10-08 11:33:23 +0200

    Rectified Home View to take in account the twig heritage

--- app/Controller/Home.php
+++ app/Controller/Home.php
@@ -8,10 +8,9 @@ class Home extends BaseController
 {
     public function home()
     {
-        ob_start();
+        
         $this->view('home.html.twig',['name' => 'Jerome']);        
-        $content = ob_get_clean();
-        require(dirname(__DIR__).'/templates/layouts/default.php'); 
+        
     }
 
     public function login()
