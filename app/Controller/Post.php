b04f8f3482887232fb4e529ea6dfd0e6d216803a
Jerome Dubus <dubus.jerome@gmail.com>
2023-11-17 15:11:32 +0100

    codacy issues

--- app/Controller/Post.php
+++ app/Controller/Post.php
@@ -68,11 +68,11 @@ class Post extends BaseController
         $perPage = null;
 
         if (isset(($this->getRoute()->getParams())['page'])) {
-            $currentPage = (int)($this->getRoute()->getParams())['page'];
+            $currentPage = (int) ($this->getRoute()->getParams())['page'];
         }
 
         if (isset(($this->getRoute()->getParams())['perPage'])) {
-            $perPage = (int)($this->getRoute()->getParams())['perPage'];
+            $perPage = (int) ($this->getRoute()->getParams())['perPage'];
         }
 
         $filter = new FilterBuilder(Application::getFilter(), substr(strtolower($this->getRoute()->getcontroller()), strrpos($this->getRoute()->getcontroller(), "\\") + 1));
